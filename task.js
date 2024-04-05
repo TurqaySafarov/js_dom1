f23f82362cc166acaf83be31e302c330d1778f2f
TurqaySafarov <161557826+TurqaySafarov@users.noreply.github.com>
2024-04-05 12:17:27 +0400

    first commit

---
+++ task.js
@@ -0,0 +1,25 @@
+// ?TASK
+//? Butona clikledikde 1 den 10-na kimi gelir sonrada geri qayidir
+
+// let btn = document.getElementById("btn");
+// let count = 1;
+// let increasing = true;
+
+// btn.addEventListener("click", () => {
+//     if (increasing) {
+//         if (count < 10) {
+//             count++;
+//         } else {
+//             increasing = false;
+//         }
+//     } else {
+//         if (count > 1) {
+//             count--;
+//         } else {
+//             increasing = true;
+//         }
+//     }
+//     btn.textContent = `Click me ${count}`;
+// });
+
+// ?###########################################################################################
